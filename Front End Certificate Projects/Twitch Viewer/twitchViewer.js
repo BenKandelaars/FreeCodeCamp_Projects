3aecf19cd646a8cb0bc8d5043ffdd05cc6665835
Ben <dutchpossum@hotmail.com>
2017-03-20 22:42:37 +0000

    Developing Twitch Viewer
    Created, then refactored code to have a general purpose jsonP Twitch API
    call function that is curried.

--- Front End Certificate Projects/Twitch Viewer/twitchViewer.js
+++ Front End Certificate Projects/Twitch Viewer/twitchViewer.js
@@ -1,21 +1,10 @@
-function jsonpCall(url, callback){
-  const script = document.createElement("script");
-  script.src = url;
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
+/*
+function displayData(data){
 
-function setUrl(channel, callback){
-  let url = "https://wind-bow.gomix.me/twitch-api/streams/"
-  url += channel
-  url += "?callback=" + String(callback);
-  return url
-}
-
-function display(data){
   console.log(data)
   if (data.stream === null){
 
-    console.log("not on line")
+    return "Channel not on line"
 
   } else {
 
@@ -27,6 +16,150 @@ function display(data){
   }
 }
 
-let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+*/
+let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"]
 
-jsonpCall(setUrl(channels[0], "display"))
+const $displayList = $(".listContainer")
+let $listItem = $("<li class=\"streamer\"></li>")
+
+function listUser(channel){
+
+  let $listItem = $("<li class=\"streamer\"></li>")
+  $listItem.attr("data", channel)
+  $listItem.append("<div class=\"logo\"></div>")
+
+  let $streamerText = $("<div class=\"streamer_info_container\"></div>")
+  $streamerText.append("<a href=\"#\"> <h3>" + channel + "</h3> </a>")
+  $streamerText.append("<div><p class=\"streamer_info\">Streamer off line</p></div>")
+
+  $listItem.append($streamerText)
+
+  $displayList.append($listItem)
+
+}
+
+
+function updateUserRegistered(data){
+
+  console.log(data.response)
+
+/*
+  let $item = $displayList.find("[data='" + data.channel + "']")
+  let $streamerInfo = $item.find("p")
+
+  let online = !!data.response.stream
+
+  // User isn't currently streaming
+  if (!online){
+    if ($item.hasClass("online")){$item.removeClass("online")}
+    $streamerInfo.text("Off line")
+  } else {
+
+  // User is streaming content
+  $streamerInfo.text(data.response.stream.game)
+
+  if (!$item.hasClass("online")){
+    $item.addClass("online")
+  }
+
+  //Update link href
+  let $link = $item.find("a")
+  $link.attr("href", data.response.stream.channel.url)
+  $link.attr("target", "_blank")
+  }
+
+  */
+}
+
+function updateUserStreaming(data){
+
+  console.log(data.response)
+
+  let $item = $displayList.find("[data='" + data.channel + "']")
+  let $streamerInfo = $item.find("p")
+
+  let online = !!data.response.stream
+
+  // User isn't currently streaming
+  if (!online){
+    if ($item.hasClass("online")){$item.removeClass("online")}
+    $streamerInfo.text("Off line")
+  } else {
+
+  // User is streaming content
+  $streamerInfo.text(data.response.stream.game)
+
+  if (!$item.hasClass("online")){
+    $item.addClass("online")
+  }
+
+  //Update link href
+  let $link = $item.find("a")
+  $link.attr("href", data.response.stream.channel.url)
+  $link.attr("target", "_blank")
+  }
+}
+
+let twitchAPICall = (function (window){
+
+  return function twitchAPICall(apifocus){
+
+    return function (channel){
+
+      let url = "https://wind-bow.gomix.me/twitch-api/"
+      url += apifocus + "/"
+      url += channel
+      url += "?callback=" + String(channel);
+
+      const script = document.createElement("script");
+      script.src = url;
+      document.getElementsByTagName("head")[0].appendChild(script);
+
+      return new Promise ((resolve, reject) => {
+
+        window[channel] = function (response){
+
+          script.remove()
+          delete window[channel]
+
+          resolve({
+            channel: channel,
+            response: response
+          })
+        }
+      })
+    }
+  }
+})(window)
+
+let checkChannel = twitchAPICall("streams")
+let checkUser = twitchAPICall("users")
+
+function initList (channels){
+
+  channels.map(function(channel){
+    listUser(channel)
+    return channel
+  })
+
+  channels.map(function (channel){
+
+    checkUser(channel)
+    .then((data) => {
+      return updateUserRegistered(data)
+    })
+  })
+
+/*
+  channels.map(function (channel){
+
+    checkChannel(channel)
+    .then((data) => {
+      return updateUserStreaming(data)
+    })
+  })
+
+*/
+}
+
+initList(channels)
